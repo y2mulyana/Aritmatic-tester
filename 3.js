c72ceac532afe86b2acbfc4c8bc99e325e53f601
y2mulyana <96508820+y2mulyana@users.noreply.github.com>
2022-07-18 23:55:15 +0700

    Remove unused

--- 3.js
+++ 3.js
@@ -7,18 +7,7 @@ let results =[];
 
 const oper = ["+", "-"];
 
-// var toke = document.getElementById("demo");
-// toke.innerHTML = generator(min, max);
-// console.log('toke : ' + toke);
-
-// let toke = generator(min, max);
-// document.getElementById("demo").innerHTML = toke;
-// qsList.push(toke);
-// console.log('toke : ' + toke);
 document.getElementById("box").value = 8;
-//document.getElementById("qty").innerHTML = '0';
-
-//qsList.push(toke);
 
 function generator(min, max) {
   const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
@@ -26,12 +15,9 @@ function generator(min, max) {
   let x = random(min, max);
   let y = random(min, max);
 
-  //let z = random(min, max);
   let o = oper[Math.floor(Math.random() * oper.length)];
 
   return String(x) + " " + String(o) + " " + String(y);
-
-  //return String(x) +  ' + '  + String(y);
 }
 
 let count = 0;
@@ -39,22 +25,17 @@ let count = 0;
 function answerForm() {
   pt = count ++;
   console.log('Count : ' + pt);
-  //document.getElementById("qty").innerHTML = count;
+  
 
   qsbox = document.getElementById("demo").innerHTML = generator(min, max);
   boxvalue = document.getElementById("box").value;
 
   ansList.push(boxvalue);
-  //console.log(boxvalue);
+ 
   qsList.push(qsbox);
 
   //================ This code u/ checking =========
-  
-
-  //document.getElementById("qs").innerHTML = qsList.toString();
-  //document.getElementById("tes").innerHTML = ansList.toString();
-
-  document.getElementById("box").value = "";
+   document.getElementById("box").value = "";
   document.getElementById('qty').innerHTML = 'Soal ke : '+pt;
 
   console.log(ansList);
@@ -62,7 +43,7 @@ function answerForm() {
   console.log('qsbox : ' +qsbox);
 
 
-  // Cek jawaban
+  // ========= Cek jawaban =========
 
   let regex = /([\+\-])/g;
 
